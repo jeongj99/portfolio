31e56bfd666fbac1bd61bf86e1fa3a1e9bc17195
Jae Yun Jeong <jeongalex0616@gmail.com>
2024-03-24 18:20:03 -0400

    Implement home component

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,17 @@
+const Social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://www.linkedin.com/in/jeongj99/" className="home__social-icon" target="_blank">
+        <i className="uil uil-linkedin-alt" />
+      </a>
+      <a href="https://github.com/jeongj99" className="home__social-icon" target="_blank">
+        <i className="uil uil-github-alt" />
+      </a>
+      <a href="https://www.instagram.com/jj.alex99" className="home__social-icon" target="_blank">
+        <i className="uil uil-instagram" />
+      </a>
+    </div>
+  );
+};
+
+export default Social;
