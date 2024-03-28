6fab2e713c22cf5ed896a3881ca5d9fc98c8895e
Jae Yun Jeong <jeongalex0616@gmail.com>
2024-03-28 16:51:14 -0400

    Finish scrollup component

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Skills from "./components/skills/Skills";
 import Qualification from "./components/qualification/Qualification";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
+import ScrollUp from "./components/scrollup/ScrollUp";
 
 const App = () => {
   return (
@@ -18,8 +19,9 @@ const App = () => {
         <Skills />
         <Qualification />
         <Contact />
-        <Footer />
       </main>
+      <Footer />
+      <ScrollUp />
       <Toaster />
     </>
   );
