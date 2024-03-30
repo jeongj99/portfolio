5023233dae8609823168c39f7f6f3372acbf6e4e
Jae Yun Jeong <jeongalex0616@gmail.com>
2024-03-29 23:56:52 -0400

    Make a new portfolio page and create new components

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,20 +1,130 @@
 import { Link } from "react-router-dom";
-import ProjectCard from "./ProjectCard";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
+import ProjectCard from "../projectcard/ProjectCard";
 import "./projects.css";
+
+const projectData = [
+  {
+    image: "/work/3.png",
+    category: "react js",
+    name: "Nexa Website",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quis.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/4.png",
+    category: "react js",
+    name: "Solstice Website",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quis.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/2.png",
+    category: "next js",
+    name: "Lumina Website",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quis.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/1.png",
+    category: "next js",
+    name: "Evolve Website",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quis.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/3.png",
+    category: "next js",
+    name: "Ignite Website",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quis.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/4.png",
+    category: "next js",
+    name: "Envision Website",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quis.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/1.png",
+    category: "fullstack",
+    name: "Serenity Website",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quis.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/3.png",
+    category: "fullstack",
+    name: "Nova Website",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quis.",
+    link: "/",
+    github: "/",
+  },
+  {
+    image: "/work/2.png",
+    category: "fullstack",
+    name: "Zenith Website",
+    description:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque, quis.",
+    link: "/",
+    github: "/",
+  },
+];
 
 const Projects = () => {
   return (
     <section className="projects section">
       <div className="projects__container">
-        <h2 className="section__title">
-          Latest Projects
-        </h2>
-        <p className="section__subtitle">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
-        </p>
-        <Link className="button button-flex" to="/portfolio">
-          All Projects
-        </Link>
+        <div className="projects__content">
+          <h2 className="section__title">Latest Projects</h2>
+          <p className="section__subtitle">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+          <Link className="button button-flex" to="/portfolio">
+            All Projects
+          </Link>
+        </div>
+        <div className="projects__swiper-container">
+          <Swiper
+            className="swiper"
+            slidesPerView={1}
+            breakpoints={{
+              640: {
+                slidesPerView: 2,
+              },
+            }}
+            spaceBetween={30}
+            modules={[Pagination]}
+            pagination={{ clickable: true }}
+          >
+            {projectData.slice(0, 4).map((project, index) => {
+              return (
+                <SwiperSlide key={index}>
+                  <ProjectCard project={project} />
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </div>
       </div>
     </section>
   );
